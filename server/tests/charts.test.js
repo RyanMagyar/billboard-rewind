48270ce9458244f3fc8985a6952a7a60b3552dbe
RyanMagyar <ramagyar@umich.edu>
2025-03-04 23:17:11 -0500

    added unit tests for error handling in charts route

--- server/tests/charts.test.js
+++ server/tests/charts.test.js
@@ -16,4 +16,47 @@ describe("GET /charts/getChart", () => {
       expect(response.body).toHaveProperty("message", "Missing query params!");
     }
   });
+
+  test("Should return 400 for improper chart name", async () => {
+    const queries = [
+      { chart: "Grunge", date: "1961-01-01" },
+      { chart: "Hello", date: "1961-01-01" },
+      { chart: "rock", date: "1961-01-01" },
+    ];
+
+    for (const query of queries) {
+      const response = await request(app).get("/charts/getChart").query(query);
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty("message", "Improper chart name.");
+    }
+  });
+
+  test("Should return 400 for incorrect date format", async () => {
+    const queries = [
+      { chart: "Rock", date: "2024-00-20" },
+      { chart: "Rock", date: "2021-1-31" },
+      { chart: "Rock", date: "2012-10-1" },
+      { chart: "Rock", date: "2012-13-10" },
+    ];
+    for (const query of queries) {
+      const response = await request(app).get("/charts/getChart").query(query);
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty("message", "Improper date format.");
+    }
+  });
+
+  test("Should return 400 for Error retrieving chart data", async () => {
+    const queries = [{ chart: "Rock", date: "2030-01-01" }];
+    for (const query of queries) {
+      const response = await request(app).get("/charts/getChart").query(query);
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty(
+        "message",
+        "Error retrieving chart data."
+      );
+    }
+  });
 });
