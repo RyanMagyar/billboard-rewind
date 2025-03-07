021047eaaabccdd2422843c989ae05a944be4303
RyanMagyar <ramagyar@umich.edu>
2025-03-07 18:35:30 -0500

    added postgres service to docker setup

--- server/db.js
+++ server/db.js
@@ -3,12 +3,18 @@ require("dotenv").config({
   path: process.env.NODE_ENV === "prod" ? ".env.prod" : ".env.dev",
 });
 
+/*
 const pool = new Pool({
   user: process.env.POSTGRESQL_USER,
   password: process.env.POSTGRESQL_PASSWORD,
   host: process.env.POSTGRESQL_HOST,
   port: process.env.POSTGRESQL_PORT, // default Postgres port
   database: process.env.POSTGRESQL_DB,
+});
+*/
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = {
