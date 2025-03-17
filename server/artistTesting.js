714179ad4f4c78e564104ba03c3bb4d204f3cd52
RyanMagyar <ramagyar@umich.edu>
2025-03-16 21:37:26 -0400

    Wrote logic for getting chart history info for artist + error handling

--- server/artistTesting.js
+++ server/artistTesting.js
@@ -1,27 +1,51 @@
-const request = require("request");
+const axios = require("axios");
 const cheerio = require("cheerio");
 
 const BILLBOARD_BASE_URL = "http://www.billboard.com";
 const BILLBOARD_ARTIST_URL = `${BILLBOARD_BASE_URL}/artist/`;
 
 async function getArtist(name, cb) {
-  let artist = name.replace(/ /g, "-");
-  console.log("Artist: " + artist);
+  try {
+    let artist = name.replace(/ /g, "-");
+    console.log("Artist: " + artist);
 
-  const requestURL = `${BILLBOARD_ARTIST_URL}${artist}/chart-history/hsi/`;
-  console.log("Request Url: " + requestURL);
+    const requestURL = `${BILLBOARD_ARTIST_URL}${artist}/chart-history/hsi/`;
+    console.log("Request Url: " + requestURL);
 
-  request(requestURL, (error, response, html) => {
-    if (error) {
-      console.log(error);
-      callback(error, null);
-      return;
-    }
+    const response = await axios.get(requestURL);
+    const html = response.data;
 
     const $ = cheerio.load(html);
 
-    console.log($);
-  });
+    const chartHeadings = $(".lrv-a-grid");
+
+    const chart = {};
+
+    chart.artist = $(".c-heading").first().text().trim();
+
+    if ($(chartHeadings[1]).find(".artist-stat-1").length > 0) {
+      // No.1 Hits
+      chart.numOnes = $(chartHeadings[1]).find(".artist-stat-1").text().trim();
+      console.log("No.1 Hits: " + chart.numOnes);
+
+      // Top 10 Hits
+      chart.topTens = $(chartHeadings[1]).find(".artist-stat-2").text().trim();
+      console.log("Top 10 Hits: " + chart.topTens);
+      // Songs
+      chart.numSongs = $(chartHeadings[1]).find(".artist-stat-3").text().trim();
+      console.log("Songs: " + chart.numSongs);
+      // Artist Name
+      console.log("Artist: " + chart.artist);
+    } else {
+      console.log(chart.artist + " has no songs to display");
+    }
+
+    if (cb) cb(null, chartHeadings);
+    return chartHeadings;
+  } catch (error) {
+    console.log(error);
+    if (cb) cb(error, null);
+  }
 }
 
 async function main() {
